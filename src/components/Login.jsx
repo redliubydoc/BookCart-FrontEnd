3411170e6ade13750cdf71a7f18ff7e7d92de635
CodyBuilder <rajeswar.sarkar.2009@gmail.com>
2022-06-26 17:52:23 +0530

    decoupled alert code from Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,6 @@ import {
 } from "react";
 
 import withLocation from "../hocs/withLocation";
-
 import LoginFrom from "./LoginForm";
 import RegisterFrom from "./RegisterFrom";
 
@@ -21,22 +20,13 @@ class Login extends Component {
 
             alert: {
                 show: false,
-                type: "",
+                level: "",
                 msg: "",
             }
         }
 
         this.toggler = this.toggler.bind(this);
         this.autoHideAlert = this.autoHideAlert.bind(this);
-    }
-
-    toggler(e) {
-        if (e.target.name === "btn-login" && !this.state.showLogin) { // toggle to Login Tab if not already in Login Tab
-            this.setState({showLogin: true, showRegister: false});
-        }
-        else if (e.target.name === "btn-register" && !this.state.showRegister) { // toggle to Register Tab if not already in Register Tab
-            this.setState({showLogin: false, showRegister: true});
-        }
     }
 
     render() {
@@ -71,18 +61,14 @@ class Login extends Component {
                         </li>
                     </ul>
                 </div>
-                    
+
                 {/* alert placeholder */}
                 <div className="container">
                     {this.state.alert.show && 
-                        <div className={`alert alert-${this.state.alert.type} d-flex align-items-center`} role="alert">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                            </svg>
-                            <span> {this.state.alert.msg} </span>
-                        </div>
-                    }     
-                </div>  
+                        <Alert level={this.state.alert.level} msg={this.state.alert.msg}/>
+                    }
+                </div>
+
                 {/* login/registration from placeholder */}
                 <div className="container-fluid">
                     { this.state.showLogin && <LoginFrom/> }
@@ -93,8 +79,8 @@ class Login extends Component {
     }
 
     componentDidMount() {
-        if (!!this.props.location.state && !!this.props.location.state.alert) { // to show alerts after redirection to login page
-            console.log(this.props.location.state);
+        // to show alerts after redirection to login page
+        if (!!this.props.location.state && !!this.props.location.state.alert) { 
             this.setState({
                     alert: this.props.location.state.alert,
                     showLogin: true,
@@ -104,11 +90,20 @@ class Login extends Component {
         }
     }
     
-    autoHideAlert(time) { // to auto hide alert after a given time
+    toggler(e) {
+        if (e.target.name === "btn-login" && !this.state.showLogin) { // toggle to Login Tab if not already in Login Tab
+            this.setState({showLogin: true, showRegister: false});
+        }
+        else if (e.target.name === "btn-register" && !this.state.showRegister) { // toggle to Register Tab if not already in Register Tab
+            this.setState({showLogin: false, showRegister: true});
+        }
+    }
+    // to auto hide alert after a given time
+    autoHideAlert(time) {
         setTimeout(() => this.setState({
             alert: {
                 show: false,
-                type: "",
+                level: "",
                 msg: ""
             }}), time * 1000);
     }
